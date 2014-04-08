9b07f3b24330281171126a3452a9f95a230afbbb
yankee <yzeugs@cipher-code.de>
2014-04-08 21:45:12 +0200

    + Added Jasmin skeleton (state of #5 at 08:38)

--- Compiler/src/de/letsbuildacompiler/compiler/Main.java
+++ Compiler/src/de/letsbuildacompiler/compiler/Main.java
@@ -12,12 +12,31 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		ANTLRInputStream input = new ANTLRFileStream("code.demo");
+		System.out.println(compile(input));
+	}
+	
+	public static String compile(ANTLRInputStream input) {
 		DemoLexer lexer = new DemoLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 		DemoParser parser = new DemoParser(tokens);
 		
 		ParseTree tree = parser.addition();
-		System.out.println(new MyVisitor().visit(tree));
+		return createJasminFile(new MyVisitor().visit(tree));
 	}
 	
+	private static String createJasminFile(String instructions) {
+		return ".class public HelloWorld\n" + 
+				".super java/lang/Object\n" + 
+				"\n" + 
+				".method public static main([Ljava/lang/String;)V\n" + 
+				"  .limit stack 100\n" + 
+				"  .limit locals 100\n" + 
+				"  \n" + 
+				"  getstatic java/lang/System/out Ljava/io/PrintStream;\n" + 
+				 instructions + "\n" + 
+				"  invokevirtual java/io/PrintStream/println(I)V\n" + 
+				"  return\n" + 
+				"  \n" + 
+				".end method";
+	}
 }
